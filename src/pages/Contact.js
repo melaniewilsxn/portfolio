58e7e8a048c4c5ba4dcdfe2487751b44fa48dfd8
Melanie Wilson <melaniemwilson1@gmail.com>
2024-08-07 15:02:42 -0400

    bug fixes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,7 +13,7 @@ function Contact(){
                     <h1 className="contact-title">Let's get in touch...</h1>
                     <p className="contact-description">Have an opportunity or just want to say hi? I'd love to hear from you!</p>
                     <p className="contact-online">
-                        Email: <a href="mailto:melaniemwilson1@gmail.com">melanieimwilson1@gmail.com</a><br />
+                        Email: <a href="mailto:melaniemwilson1@gmail.com">melaniemwilson1@gmail.com</a><br />
                         Online: <a href="https://www.linkedin.com/in/melanie-marie-wilson/" target="_blank" rel="noopener noreferrer">LinkedIn</a> /
                         <a href="https://github.com/melaniewilsxn" target="_blank" rel="noopener noreferrer"> GitHub</a> /
                         <a href="https://www.instagram.com/melaniewilsxn/" target="_blank" rel="noopener noreferrer"> Instagram</a>
